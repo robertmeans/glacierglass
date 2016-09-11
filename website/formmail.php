954bfbc0eb648f18e2f4b03884448456d17aa7e0
robertmeans <robert@robertmeans.com>
2016-09-11 13:26:17 -0600

    1st Site Launch - (no really!)
    had to make several additional modificiations in order to tighten
    everything up a little before actual launch.

--- website/formmail.php
+++ website/formmail.php
@@ -94,22 +94,7 @@ mail($my_email,$subject,$message,$headers);
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="cache-control" content="no-cache">
-	<meta http-equiv="pragma" content="no-cache">
-	<meta http-equiv="expires" content="0">
-	
-	<title>Glacier Glass</title>
-	<!-- <link rel="icon" type="image/ico" href="_images/favicon.ico"> -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
-	<link href='https://fonts.googleapis.com/css?family=Prompt|Roboto:300i,400|Lato' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="style.css?<?php echo time(); ?>" type="text/css">
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-</head>
+<?php include "_includes/head.php" ?>
 <body class="thank-you-page">
 <div id="wrapper">
 
